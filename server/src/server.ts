8d49637ec561a2da2818c3ca3b05c68e1e3e34a3
Nicolas Elena <37024797+NicolasElena@users.noreply.github.com>
2020-09-14 14:48:46 -0300

    base das rotas get post

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,37 @@ import express, { response } from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('aaaa');
+app.use(express.json());
 
-  response.json(['asad', 'asd']);
+const users = ['asad', 'asd', 'aaaa'];
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
