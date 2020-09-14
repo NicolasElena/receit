ef818874c67fbea640d8dfee58ffbf744d4879c5
Nicolas Elena <37024797+NicolasElena@users.noreply.github.com>
2020-09-14 15:15:37 -0300

    arquivo exclusivo para as rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,10 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const users = ['asad', 'asd', 'aaaa'];
-
-app.get('/users', (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
