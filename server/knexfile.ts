6891a4c7af6979738aec8f98b929e6c3393b90a3
Nicolas Elena <37024797+NicolasElena@users.noreply.github.com>
2020-09-15 15:16:17 -0300

    migrations e knexfile

---
+++ server/knexfile.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  client: 'pg',
+  connection: {
+    database: 'receit_bd',
+    user: 'postgres',
+    password: 'N!cobd',
+  },
+  migrations: {
+    directory: `${__dirname}/src/database/migrations`,
+  },
+};
