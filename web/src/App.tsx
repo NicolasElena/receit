35a4c485e9d47f55274990af742999f1a61111e4
Matheus Rodrigues <36890610+Th3Matts@users.noreply.github.com>
2020-09-17 01:07:00 -0300

    Login screen finalizado (Ou quase!)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import logoImg from './assets/Images/logo-min.png'
+
+import './assets/Styles/global.css'
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>Hello world</h1>
-      </header>
-    </div>
+  return(
+    <main id="login">
+        <div className="logo-container">
+          <img src={logoImg} alt="Receit"/>
+      </div>
+      <div className="loginTxt">
+        <h1>Login</h1>
+      </div>
+
+      <div className="input-block" >
+        <input type="text" id="login" />
+        <input type="text" id="password" />
+      </div>
+
+      <div className="submit-button">
+        <button type="submit"> 
+          Entrar
+        </button>
+      </div>
+    </main>
   );
 }
 
---
+++ web/src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div id="page-login">
+      
+    </div>
+  )
+}
+
+export default Login;
