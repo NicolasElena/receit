5fa9517a144f38e73711ce1443adba3cad2964d8
Matheus Rodrigues <36890610+Th3Matts@users.noreply.github.com>
2020-09-29 21:05:57 -0300

    Login screen finalizado!

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 
 import logoImg from './assets/Images/logo-min.png'
+import dsktpBG from './assets/Images/Desktop_bg.svg'
 
 import './assets/Styles/global.css'
 
 function App() {
   return(
     <main id="login">
+      <div id="login-container">
         <div className="logo-container">
           <img src={logoImg} alt="Receit"/>
-      </div>
-      <div className="loginTxt">
-        <h1>Login</h1>
-      </div>
+        </div>
+        <div className="loginTxt">
+          <h1>Login</h1>
+        </div>
 
-      <div className="input-block" >
-        <input type="text" id="login" />
-        <input type="text" id="password" />
-      </div>
+        <div className="input-block" >
+          <input type="text" placeholder="E-mail" id="login" />
+          <input type="password" placeholder="Password" id="password" />
+        </div>
 
-      <div className="submit-button">
-        <button type="submit"> 
-          Entrar
-        </button>
+        <div className="submit-button">
+          <button type="submit"> 
+            Entrar
+          </button>
+        </div>
       </div>
     </main>
   );
