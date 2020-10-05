c182b8efdb06efe0c8e59d7f65f880fde0cc0b01
Nicolas Elena <37024797+NicolasElena@users.noreply.github.com>
2020-10-05 13:00:08 -0300

    Página no lugar certo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import logoImg from './assets/Images/logo-min.png'
-import dsktpBG from './assets/Images/Desktop_bg.svg'
+// import dsktpBG from './assets/Images/Desktop_bg.svg'
+import Login from './pages/Login';
 
-import './assets/Styles/global.css'
+import './assets/Styles/global.css';
 
 function App() {
-  return(
-    <main id="login">
-      <div id="login-container">
-        <div className="logo-container">
-          <img src={logoImg} alt="Receit"/>
-        </div>
-        <div className="loginTxt">
-          <h1>Login</h1>
-        </div>
-
-        <div className="input-block" >
-          <input type="text" placeholder="E-mail" id="login" />
-          <input type="password" placeholder="Password" id="password" />
-        </div>
-
-        <div className="submit-button">
-          <button type="submit"> 
-            Entrar
-          </button>
-        </div>
-      </div>
-    </main>
-  );
+  return <Login />;
 }
 
 export default App;
--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-function Login() {
+import logoImg from '../../assets/Images/logo-min.png';
+import './styles.css';
+
+interface LoginProps {}
+
+const Login: React.FC = () => {
   return (
-    <div id="page-login">
-      
+    <div className='bg'>
+      <div className='login'>
+        <div className='logo-container'>
+          <img src={logoImg} alt='Receit' />
+        </div>
+        <div className='loginTxt'>
+          <h1>Login</h1>
+        </div>
+
+        <div className='input-block'>
+          <div>
+            <input type='text' placeholder='E-mail' id='login' />
+          </div>
+          <div>
+            <input type='password' placeholder='Password' id='password' />
+          </div>
+        </div>
+
+        <div className='submit-button'>
+          <button type='submit'>Entrar</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
