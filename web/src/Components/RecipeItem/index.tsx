a3e4e8f25479fe28c61011d05423498cca9a2e57
Matheus Rodrigues <36890610+Th3Matts@users.noreply.github.com>
2020-11-11 15:59:23 -0300

    Criação do componente receitas

---
+++ web/src/Components/RecipeItem/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import './styles.css'
+
+import recipeImg from '../../assets/Images/Media.png'
+import forkImg from '../../assets/Images/Fork.png'
+import likeImg from '../../assets/Images/Like.png'
+import cookImg from '../../assets/Images/Cook.png'
+
+function RecipeItem() {
+  return (
+    <article className="recipe-item">
+      <header className="recipe">
+        <img src={recipeImg} alt="Recipe"/>
+      </header>
+          <h3> Nome da receita </h3>
+        <div className="recipe-things">
+          <div  className="recipe-category">
+            Categoria 1
+            Categoria 2 
+          </div>
+          <div className="recipe-ingredients">
+              Ingrediente 1
+              Ingrediente 2
+              Ingrediente 3
+          </div>
+
+        </div>
+        
+        <footer>
+          <button className="cook">
+            <img src={cookImg} alt="cook"/>
+            Nico
+          </button>
+          <button>
+            <img src={forkImg} alt="Fork"/>
+          </button>
+          <button>
+            <img src={likeImg} alt="Like"/>
+          </button>
+        </footer>
+    </article>
+  )
+}
+
+export default RecipeItem;
--- web/src/pages/Receitas/index.tsx
+++ web/src/pages/Receitas/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import './styles.css'
 import PageHeader from '../../Components/PageHeader';
+import RecipeItem from '../../Components/RecipeItem';
 
 
 function Receitas() {
@@ -14,6 +15,12 @@ function Receitas() {
           <h2> Minhas Receitas </h2>  
           <input type="search" placeholder="Search"/>
           <button> Categorias </button>
+        </div>
+
+        <div className="recipes">
+          <RecipeItem />
+          <RecipeItem />
+          <RecipeItem />
         </div>
       </div>
     </div>  
